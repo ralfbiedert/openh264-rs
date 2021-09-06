f036c99a44b0f5eaa6a7104e1e2f7f07a96241fc
Ralf Biedert <rb@xr.io>
2021-09-06 18:59:17 +0200

    Enable more tests, debugging, remove image writing.

--- openh264/tests/decode.rs
+++ openh264/tests/decode.rs
@@ -1,4 +1,3 @@
-use image::ColorType;
 use openh264::{Decoder, DecoderConfig, Error};
 
 #[test]
@@ -11,7 +10,6 @@ fn can_get_decoder() -> Result<(), Error> {
 
 #[test]
 #[rustfmt::skip]
-#[ignore]
 fn can_decode_single() -> Result<(), Error> {
     let sources = [
         &include_bytes!("data/single_1920x1080_cabac.h264")[..],
@@ -19,8 +17,8 @@ fn can_decode_single() -> Result<(), Error> {
         &include_bytes!("data/single_512x512_cavlc.h264")[..],
     ];
 
-    for (i, src) in sources.iter().enumerate() {
-        let config = DecoderConfig::default();
+    for (_, src) in sources.iter().enumerate() {
+        let config = DecoderConfig::default().debug(true);
         let mut decoder = Decoder::with_config(config)?;
 
         let yuv = decoder.decode_no_delay(src)?;
@@ -30,16 +28,6 @@ fn can_decode_single() -> Result<(), Error> {
         let mut rgb = vec![0; rgb_len];
 
         yuv.write_rgb8(&mut rgb)?;
-
-        let strides = yuv.strides_yuv();
-        let dim_y = yuv.dimension_y();
-        let dim_u = yuv.dimension_u();
-        let dim_v = yuv.dimension_v();
-
-        image::save_buffer(format!("{}_rgb.png", i), &rgb, dim.0 as u32, dim.1 as u32, ColorType::Rgb8).unwrap();
-        image::save_buffer(format!("{}_y.png", i), yuv.y_with_stride(), strides.0 as u32, dim_y.1 as u32, ColorType::L8).unwrap();
-        image::save_buffer(format!("{}_u.png", i), yuv.u_with_stride(), strides.1 as u32, dim_u.1 as u32, ColorType::L8).unwrap();
-        image::save_buffer(format!("{}_v.png", i), yuv.v_with_stride(), strides.2 as u32, dim_v.1 as u32, ColorType::L8).unwrap();
     }
 
     Ok(())
@@ -49,7 +37,7 @@ fn can_decode_single() -> Result<(), Error> {
 fn can_decode_multi_to_end() -> Result<(), Error> {
     let src = &include_bytes!("data/multi_512x512.h264")[..];
 
-    let config = DecoderConfig::default();
+    let config = DecoderConfig::default().debug(true);
     let mut decoder = Decoder::with_config(config)?;
 
     decoder.decode_no_delay(src)?;
@@ -63,7 +51,7 @@ fn can_decode_multi_by_step() -> Result<(), Error> {
 
     let packet_lengths = [30, 2736, 2688, 2672, 2912, 3215];
 
-    let config = DecoderConfig::default();
+    let config = DecoderConfig::default().debug(true);
     let mut decoder = Decoder::with_config(config)?;
 
     let mut p = 0;
@@ -81,7 +69,7 @@ fn can_decode_multi_by_step() -> Result<(), Error> {
 fn fails_on_truncated() -> Result<(), Error> {
     let src = &include_bytes!("data/multi_512x512_truncated.h264")[..];
 
-    let config = DecoderConfig::default();
+    let config = DecoderConfig::default().debug(true);
     let mut decoder = Decoder::with_config(config)?;
 
     assert!(decoder.decode_no_delay(src).is_err());
