0d6c1e198b9fda68407fc696c8fbd217cbfdab3f
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-09-21 23:36:07 -0400

    Add encoding impl

--- openh264-sys2/build.rs
+++ openh264-sys2/build.rs
@@ -19,51 +19,55 @@ fn main() {
         debug = true;
         opt_level = 0;
     }
+    if cfg!(feature = "decoder") {
+        cc::Build::new()
+            .include("upstream/codec/api/svc/")
+            .include("upstream/codec/common/inc/")
+            .include("upstream/codec/decoder/core/inc/")
+            .include("upstream/codec/decoder/plus/inc/")
+            .include("upstream/codec/processing/interface/")
+            .files(ugly_cpp_import("upstream/codec/common"))
+            .files(ugly_cpp_import("upstream/codec/decoder"))
+            .cpp(true)
+            .warnings(false)
+            .opt_level(opt_level)
+            .pic(true)
+            // Upstream sets these two and if we don't we get segmentation faults on Linux and MacOS ... Happy times.
+            .flag_if_supported("-fno-strict-aliasing")
+            .flag_if_supported("-fstack-protector-all")
+            .flag_if_supported("-fembed-bitcode")
+            .flag_if_supported("-fno-common")
+            .flag_if_supported("-undefined dynamic_lookup")
+            .debug(debug)
+            .compile("libopenh264_decode.a");
 
-    cc::Build::new()
-        .include("upstream/codec/api/svc/")
-        .include("upstream/codec/common/inc/")
-        .include("upstream/codec/decoder/core/inc/")
-        .include("upstream/codec/decoder/plus/inc/")
-        .include("upstream/codec/processing/interface/")
-        .files(ugly_cpp_import("upstream/codec/common"))
-        .files(ugly_cpp_import("upstream/codec/decoder"))
-        .cpp(true)
-        .warnings(false)
-        .opt_level(opt_level)
-        .pic(true)
-        // Upstream sets these two and if we don't we get segmentation faults on Linux and MacOS ... Happy times.
-        .flag_if_supported("-fno-strict-aliasing")
-        .flag_if_supported("-fstack-protector-all")
-        .flag_if_supported("-fembed-bitcode")
-        .flag_if_supported("-fno-common")
-        .flag_if_supported("-undefined dynamic_lookup")
-        .debug(debug)
-        .compile("libopenh264_decode.a");
+        println!("cargo:rustc-link-lib=static=openh264_decode");
+    }
 
-    cc::Build::new()
-        .include("upstream/codec/api/svc/")
-        .include("upstream/codec/common/inc/")
-        .include("upstream/codec/encoder/core/inc/")
-        .include("upstream/codec/encoder/plus/inc/")
-        .include("upstream/codec/processing/interface/")
-        .include("upstream/codec/processing/src/common/")
-        .files(ugly_cpp_import("upstream/codec/encoder"))
-        .files(ugly_cpp_import("upstream/codec/processing"))
-        .cpp(true)
-        .warnings(false)
-        .opt_level(opt_level)
-        .pic(true)
-        // Upstream sets these two and if we don't we get segmentation faults on Linux and MacOS ... Happy times.
-        .flag_if_supported("-fno-strict-aliasing")
-        .flag_if_supported("-fstack-protector-all")
-        .flag_if_supported("-fembed-bitcode")
-        .flag_if_supported("-fno-common")
-        .flag_if_supported("-undefined dynamic_lookup")
-        .debug(debug)
-        // .cpp_link_stdlib("c++_static")
-        .compile("libopenh264_encode.a");
+    if cfg!(feature = "encoder") {
+        cc::Build::new()
+            .include("upstream/codec/api/svc/")
+            .include("upstream/codec/common/inc/")
+            .include("upstream/codec/encoder/core/inc/")
+            .include("upstream/codec/encoder/plus/inc/")
+            .include("upstream/codec/processing/interface/")
+            .include("upstream/codec/processing/src/common/")
+            .files(ugly_cpp_import("upstream/codec/encoder"))
+            .files(ugly_cpp_import("upstream/codec/processing"))
+            .cpp(true)
+            .warnings(false)
+            .opt_level(opt_level)
+            .pic(true)
+            // Upstream sets these two and if we don't we get segmentation faults on Linux and MacOS ... Happy times.
+            .flag_if_supported("-fno-strict-aliasing")
+            .flag_if_supported("-fstack-protector-all")
+            .flag_if_supported("-fembed-bitcode")
+            .flag_if_supported("-fno-common")
+            .flag_if_supported("-undefined dynamic_lookup")
+            .debug(debug)
+            // .cpp_link_stdlib("c++_static")
+            .compile("libopenh264_encode.a");
 
-    println!("cargo:rustc-link-lib=static=openh264_encode");
-    println!("cargo:rustc-link-lib=static=openh264_decode");
+        println!("cargo:rustc-link-lib=static=openh264_encode");
+    }
 }
--- openh264/tests/encode.rs
+++ openh264/tests/encode.rs
@@ -1,5 +1,9 @@
+#![cfg(feature = "encoder")]
+
+#[cfg(feature = "decoder")]
 use openh264::decoder::{Decoder, DecoderConfig};
-use openh264::encoder::{Encoder, EncoderConfig};
+
+use openh264::encoder::{Encoder, EncoderConfig, RBGYUVConverter};
 use openh264::Error;
 
 #[test]
@@ -10,28 +14,34 @@ fn can_get_encoder() -> Result<(), Error> {
     Ok(())
 }
 
-// Encode function broken for now.
-#[ignore]
 #[test]
+fn encode() -> Result<(), Error> {
+    let src = &include_bytes!("data/lenna_128x128.rgb")[..];
+
+    let config = EncoderConfig::new(128, 128);
+    let mut encoder = Encoder::with_config(config)?;
+    let mut converter = RBGYUVConverter::new(128, 128);
+
+    converter.convert(src);
+    encoder.encode(&converter)?;
+
+    Ok(())
+}
+
+// Encode function broken for now.
+#[test]
+#[cfg(all(feature = "decoder", feature = "encoder"))]
 fn what_goes_around_comes_around() -> Result<(), Error> {
     let src = &include_bytes!("data/single_512x512_cavlc.h264")[..];
 
-    let config = DecoderConfig::default().debug(true);
+    let config = DecoderConfig::default();
     let mut decoder = Decoder::with_config(config)?;
     let yuv = decoder.decode_no_delay(src)?;
-    let strides = yuv.strides_yuv();
 
     let config = EncoderConfig::new(512, 512);
     let mut encoder = Encoder::with_config(config)?;
 
-    encoder.encode_todo(
-        yuv.v_with_stride(),
-        yuv.u_with_stride(),
-        yuv.v_with_stride(),
-        strides.0,
-        strides.1,
-        strides.2,
-    )?;
+    encoder.encode(&yuv)?;
 
     Ok(())
 }
