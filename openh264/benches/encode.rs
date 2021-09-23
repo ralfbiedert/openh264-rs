6f28770fda82282d0bba7a8d144cef3a4c6da9be
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-09-22 22:52:39 -0400

    Add more benchmarks for encoder and yuv conversion

--- openh264/benches/encode.rs
+++ openh264/benches/encode.rs
@@ -7,7 +7,7 @@ use openh264::encoder::{Encoder, EncoderConfig};
 use test::{black_box, Bencher};
 
 #[bench]
-fn encode_first_512x512_from_yuv(b: &mut Bencher) {
+fn encode_512x512_from_yuv(b: &mut Bencher) {
     let source = include_bytes!("../tests/data/single_512x512_cabac.h264");
 
     let config = DecoderConfig::default();
@@ -16,6 +16,24 @@ fn encode_first_512x512_from_yuv(b: &mut Bencher) {
 
     b.iter(|| {
         let config = EncoderConfig::new(512, 512);
+        let mut encoder = Encoder::with_config(config).unwrap();
+
+        let stream = encoder.encode(&yuv).unwrap();
+
+        black_box(stream);
+    });
+}
+
+#[bench]
+fn encode_1920x1080_from_yuv(b: &mut Bencher) {
+    let source = include_bytes!("../tests/data/single_1920x1080_cabac.h264");
+
+    let config = DecoderConfig::default();
+    let mut decoder = Decoder::with_config(config).unwrap();
+    let yuv = decoder.decode_no_delay(source).unwrap();
+
+    b.iter(|| {
+        let config = EncoderConfig::new(1920, 1080);
         let mut encoder = Encoder::with_config(config).unwrap();
 
         let stream = encoder.encode(&yuv).unwrap();
