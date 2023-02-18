2534493b7336cb4893270b913dd78b516daab722
Nikita Strygin <nikita6@bk.ru>
2023-02-18 16:35:45 +0300

    Move all the code dependent on mp4 crate to the examples

--- openh264/examples/mp4/main.rs
+++ openh264/examples/mp4/main.rs
@@ -1,13 +1,11 @@
-#![cfg(all(feature = "decoder", feature = "mp4"))]
+mod mp4_bitstream_converter;
 
-use openh264::decoder::{Decoder, DecoderConfig};
-use openh264::{Error, Mp4BitstreamConverter};
+use crate::mp4_bitstream_converter::Mp4BitstreamConverter;
+use openh264::decoder::Decoder;
 use std::io::Cursor;
 
-#[test]
-fn can_decode_mp4_bitstream() -> Result<(), Error> {
-    let mp4 = include_bytes!("data/multi_512x512.mp4");
-
+fn main() {
+    let mp4 = include_bytes!("../../tests/data/multi_512x512.mp4");
     let mut mp4 = mp4::Mp4Reader::read_header(Cursor::new(mp4), mp4.len() as u64).unwrap();
 
     let track = mp4
@@ -22,19 +20,18 @@ fn can_decode_mp4_bitstream() -> Result<(), Error> {
     // the mp4 stream also lacks parameter sets, so we need to add them
     // Mp4BitstreamConverter does this for us
     let mut bitstream_converter = Mp4BitstreamConverter::for_mp4_track(track);
-
-    let config = DecoderConfig::default().debug(false);
-    let mut decoder = Decoder::with_config(config)?;
+    let mut decoder = Decoder::new().unwrap();
 
     let mut buffer = Vec::new();
+    let mut rgb = [0; 512 * 512 * 3];
 
     for i in 1..track.sample_count() + 1 {
         let sample = mp4.read_sample(track_id, i).unwrap().unwrap();
         // convert the packet from mp4 representation to one that openh264 can decode
         bitstream_converter.convert_packet(&sample.bytes, &mut buffer);
 
-        decoder.decode(&buffer)?;
+        if let Some(image) = decoder.decode(&buffer).unwrap() {
+            image.write_rgb8(&mut rgb);
+        }
     }
-
-    Ok(())
 }
