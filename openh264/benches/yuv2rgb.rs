418b2a667bc76f5d408f67f848e1120a0bb30909
Jannik Schleicher <jannik.schleicher@telespazio.de>
2024-12-09 13:53:50 +0100

    Clean Up - remove experimental implementations - add benchmarks for 4k resolution

--- openh264/benches/yuv2rgb.rs
+++ openh264/benches/yuv2rgb.rs
@@ -9,7 +9,7 @@ use test::Bencher;
 
 #[bench]
 #[cfg(feature = "source")]
-fn convert_yuv_to_rgb_1920x1080(b: &mut Bencher) {
+fn convert_yuv_to_rgb_1920x1080_scalar(b: &mut Bencher) {
     let source = include_bytes!("../tests/data/single_1920x1080_cabac.h264");
 
     let api = OpenH264API::from_source();
@@ -24,7 +24,28 @@ fn convert_yuv_to_rgb_1920x1080(b: &mut Bencher) {
     let tgt = &mut rgb[0..rgb_len];
 
     b.iter(|| {
-        yuv.write_rgb8(tgt);
+        openh264::decoder::DecodedYUV::write_rgb8_scalar(yuv.y(), yuv.u(), yuv.v(), yuv.dimensions(), yuv.strides(), tgt);
+    });
+}
+
+#[bench]
+#[cfg(feature = "source")]
+fn convert_yuv_to_rgb_1920x1080_f32x8(b: &mut Bencher) {
+    let source = include_bytes!("../tests/data/single_1920x1080_cabac.h264");
+
+    let api = OpenH264API::from_source();
+    let config = DecoderConfig::default();
+    let mut decoder = Decoder::with_api_config(api, config).unwrap();
+
+    let mut rgb = vec![0; 2000 * 2000 * 4];
+    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
+    let dim = yuv.dimensions();
+    let rgb_len = dim.0 * dim.1 * 4;
+
+    let tgt = &mut rgb[0..rgb_len];
+
+    b.iter(|| {
+        openh264::decoder::DecodedYUV::write_rgb8_f32x8(yuv.y(), yuv.u(), yuv.v(), yuv.dimensions(), yuv.strides(), tgt);
     });
 }
 
@@ -66,56 +87,13 @@ fn convert_yuv_to_rgb_512x512(b: &mut Bencher) {
     let tgt = &mut rgb[0..rgb_len];
 
     b.iter(|| {
-        yuv.write_rgb8(tgt);
-    });
-}
-
-
-#[bench]
-#[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_lookup(b: &mut Bencher) {
-    let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
-
-    let api = OpenH264API::from_source();
-    let config = DecoderConfig::default();
-    let mut decoder = Decoder::with_api_config(api, config).unwrap();
-
-    let mut rgb = vec![0; 2000 * 2000 * 3];
-    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
-    let dim = yuv.dimensions();
-    let rgb_len = dim.0 * dim.1 * 3;
-
-    let tgt = &mut rgb[0..rgb_len];
-
-    b.iter(|| {
-        yuv.write_rgb8_lookup(tgt);
+        openh264::decoder::DecodedYUV::write_rgb8_scalar(yuv.y(), yuv.u(), yuv.v(), yuv.dimensions(), yuv.strides(), tgt);
     });
 }
 
 #[bench]
 #[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_i16_lookup(b: &mut Bencher) {
-    let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
-
-    let api = OpenH264API::from_source();
-    let config = DecoderConfig::default();
-    let mut decoder = Decoder::with_api_config(api, config).unwrap();
-
-    let mut rgb = vec![0; 2000 * 2000 * 3];
-    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
-    let dim = yuv.dimensions();
-    let rgb_len = dim.0 * dim.1 * 3;
-
-    let tgt = &mut rgb[0..rgb_len];
-
-    b.iter(|| {
-        yuv.write_rgb8_i16_lookup(tgt);
-    });
-}
-
-#[bench]
-#[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_i16_math(b: &mut Bencher) {
+fn convert_yuv_to_rgb_512x512_f32x8(b: &mut Bencher) {
     let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
     
     let api = OpenH264API::from_source();
@@ -130,74 +108,11 @@ fn convert_yuv_to_rgb_512x512_i16_math(b: &mut Bencher) {
     let tgt = &mut rgb[0..rgb_len];
 
     b.iter(|| {
-        yuv.write_rgb8_i16_math(tgt);
+        openh264::decoder::DecodedYUV::write_rgb8_f32x8(yuv.y(), yuv.u(), yuv.v(), yuv.dimensions(), yuv.strides(), tgt);
     });
 }
 
-#[bench]
-#[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_x8(b: &mut Bencher) {
-    let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
-    
-    let api = OpenH264API::from_source();
-    let config = DecoderConfig::default();
-    let mut decoder = Decoder::with_api_config(api, config).unwrap();
-
-    let mut rgb = vec![0; 2000 * 2000 * 3];
-    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
-    let dim = yuv.dimensions();
-    let rgb_len = dim.0 * dim.1 * 3;
-
-    let tgt = &mut rgb[0..rgb_len];
-
-    b.iter(|| {
-        yuv.write_rgb8_x8(tgt);
-    });
-}
-
-#[bench]
-#[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_x8_mul_add(b: &mut Bencher) {
-    let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
-    
-    let api = OpenH264API::from_source();
-    let config = DecoderConfig::default();
-    let mut decoder = Decoder::with_api_config(api, config).unwrap();
-
-    let mut rgb = vec![0; 2000 * 2000 * 3];
-    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
-    let dim = yuv.dimensions();
-    let rgb_len = dim.0 * dim.1 * 3;
-
-    let tgt = &mut rgb[0..rgb_len];
-
-    b.iter(|| {
-        yuv.write_rgb8_x8_mul_add(tgt);
-    });
-}
-
-
-#[bench]
-#[cfg(feature = "source")]
-fn convert_yuv_to_rgb_512x512_i16x16(b: &mut Bencher) {
-    let source = include_bytes!("../tests/data/single_512x512_cavlc.h264");
-    
-    let api = OpenH264API::from_source();
-    let config = DecoderConfig::default();
-    let mut decoder = Decoder::with_api_config(api, config).unwrap();
-
-    let mut rgb = vec![0; 2000 * 2000 * 3];
-    let yuv = decoder.decode(&source[..]).unwrap().unwrap();
-    let dim = yuv.dimensions();
-    let rgb_len = dim.0 * dim.1 * 3;
-
-    let tgt = &mut rgb[0..rgb_len];
-
-    b.iter(|| {
-        yuv.write_rgb8_i16x16(tgt);
-    });
-}
-
+/// collect a Vec of random numbers within the specified range
 macro_rules! gen_range {
     ($range:expr, $count:expr) => {{
         let mut rng = rand::thread_rng();
@@ -207,85 +122,34 @@ macro_rules! gen_range {
 
 #[bench]
 #[cfg(feature = "source")]
-fn clamping_i32_u8(b: &mut Bencher) {
-    use std::hint::black_box;
-
-    let nums: Vec<i32> = gen_range!(-227..480, 512 * 512 * 3);
-    assert_eq!(512 * 512 * 3, nums.len());
+fn convert_yuv_to_rgb_4096x2160_scalar(b: &mut Bencher) {    
+    let (im_w, im_h) = (4096, 2160);
+    let (y_len, uv_len) = (im_w * im_h, (im_w * im_h) / 4);
+    let strides  = (im_w, im_w / 2, im_w / 2);
     
-    let mut dump = [0u8; 1];
+    let y_plane: Vec<u8> = gen_range!(0..=255u8, y_len);
+    let u_plane: Vec<u8> = gen_range!(0..=255u8, uv_len);
+    let v_plane: Vec<u8> = gen_range!(0..=255u8, uv_len);
+
+    let mut tgt = vec![0; im_w * im_h * 3];
     b.iter(|| {
-        for n in nums.iter() {
-            dump[0] = black_box((*n).clamp(0, 255) as u8);
-        }
+        openh264::decoder::DecodedYUV::write_rgb8_scalar(&y_plane, &u_plane, &v_plane, (im_w, im_h), strides, &mut tgt);
     });
 }
 
 #[bench]
 #[cfg(feature = "source")]
-fn clamping_f32_u8(b: &mut Bencher) {
-    use std::hint::black_box;
-
-    let nums: Vec<i32> = gen_range!(-227..480, 512 * 512 * 3);
-    assert_eq!(512 * 512 * 3, nums.len());
-
-    let mut dump = [0u8; 1];
-    b.iter(|| {
-        for n in nums.iter() {
-            // clamps implicitly
-            dump[0] = black_box((*n) as u8);
-        }
-    });
-}
-
-#[bench]
-#[cfg(feature = "source")]
-fn clamping_lookup(b: &mut Bencher) {
-    use std::hint::black_box;
+fn convert_yuv_to_rgb_4096x2160_f32x8(b: &mut Bencher) {    
+    let (im_w, im_h) = (4096, 2160);
+    let (y_len, uv_len) = (im_w * im_h, (im_w * im_h) / 4);
+    let strides  = (im_w, im_w / 2, im_w / 2);
     
-    /*
-        lookup table as described in Etienne Dupuis paper, works like hard-sigmoid function
+    let y_plane: Vec<u8> = gen_range!(0..=255u8, y_len);
+    let u_plane: Vec<u8> = gen_range!(0..=255u8, uv_len);
+    let v_plane: Vec<u8> = gen_range!(0..=255u8, uv_len);
 
-        Unclamped values:
-        yuv 0, 0, 0
-        yields rbg -180, 135, -227
-
-        yuv 128, 128, 128
-        yields rgb 128, 128, 128
-
-        yuv 255, 255, 255
-        yields rgb 433, 120, 480    
-    */
-
-    // generate a lookup table where:
-    // - the first 227 values are mapped to 0
-    // - 227 to (227 + 255) linear mapping from 0 to 255
-    // - (227 + 255) to end are mapped to 255
-    // for the all possible blue values [-227, 480], we have an indexed lookup,
-    // mapping all over/underflowing values into the valid u8 range    
-    let mut blue_lookup = [0u8; 480 + 227];
-    for (i, v) in (0..(480 + 227 as i32)).map(|i| (i - 227).clamp(0, 255) as u8).enumerate() {
-        blue_lookup[i] = v;
-    }
-
-    // index 227 is the "origin"
-    assert_eq!(0, blue_lookup[227]);
-    // values before the origin are mapped to 0
-    assert_eq!(0, blue_lookup[226]);
-    // values after the origin are mapped to positive numbers
-    assert_eq!(1, blue_lookup[228]);    
-    assert_eq!(255, blue_lookup[228 + 255]);
-    assert_eq!(255, blue_lookup[228 + 256]);
-    
-    let nums: Vec<isize> = gen_range!(-227..480, 512 * 512 * 3);
-    assert_eq!(512 * 512 * 3, nums.len());
-
-    let mut dump = [0u8; 1];
+    let mut tgt = vec![0; im_w * im_h * 3];
     b.iter(|| {
-        for i in &nums {
-            dump[0] = black_box(
-                blue_lookup[(i + 227) as usize]
-            );
-        }
+        openh264::decoder::DecodedYUV::write_rgb8_f32x8(&y_plane, &u_plane, &v_plane, (im_w, im_h), strides, &mut tgt);
     });
 }
